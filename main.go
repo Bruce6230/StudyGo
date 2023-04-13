ad7d4779f2c1498aed5e40a3cd7b1d013ff7115f
Makiyo <lin171820@gmail.com>
2023-04-13 21:20:11 +0800

    变量

--- main.go
+++
@@ -1,7 +0,0 @@
-package StudyGo
-
-import "fmt"
-
-func main() {
-	fmt.Println("hello world")
-}
---
+++ main/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//hello world
+//import "fmt"
+//
+//func main() {
+//	fmt.Println("hello world")
+//}
+
+// 定义变量 var name type = expression
+func main() {
+	var i int = 10
+	fmt.Println(i)
+	//也可以不用指定变量类型
+	var (
+		j = 0
+		k = 1
+	)
+	fmt.Println(j + k)
+	var (
+		float32 = 3.2
+		flag    = false
+		s       = "hello"
+	)
+	fmt.Println(float32, flag, s)
+	/**
+	0为变量默认
+	**/
+	//变量简短声明 结构：变量名:=表达式
+	t := 10
+	fl := false
+	fmt.Println(t, fl)
+	//指针
+	var pi = &i
+	*pi = 15
+	what := pi
+	fmt.Println(*pi + *what)
+	//常量
+	const (
+		one = iota + 1
+		two
+		three
+		four
+	)
+	fmt.Println(one, two, three, four)
+	//Go是强类型语言
+
+	//int转string
+	i2s := strconv.Itoa(i)
+	//string转int
+	s2i, err := strconv.Atoi(i2s)
+	fmt.Println(i2s, s2i, err)
+	//判断s的前缀是否是h
+	fmt.Println(strings.HasPrefix(s, "h"))
+	//再s中查找字符串l
+	fmt.Println(strings.Index(s, "l"))
+	//将s全部转为大写
+	fmt.Println(strings.ToUpper(s))
+	/**
+	len(s string) int：返回字符串s的长度。
+
+	s[i]：获取字符串s中的第i个字符。
+
+	s[i:j]：获取字符串s中从第i个字符到第j-1个字符组成的子串。
+
+	s + t：将两个字符串s和t拼接起来。
+
+	strings.Contains(s, substr string) bool：判断字符串s是否包含子串substr，返回bool类型。
+
+	strings.Index(s, sep string) int：在字符串s中查找sep第一次出现的位置，返回位置索引，如果不存在返回-1。
+
+	strings.Count(s, sep string) int：计算字符串s中sep出现的次数。
+
+	strings.Replace(s, old, new string, n int) string：将字符串s中前n个old子串替换为new子串，返回新字符串。
+
+	strings.Split(s, sep string) []string：将字符串s按照sep分隔成多个子串组成的切片。
+
+	strings.ToLower(s string) string：将字符串s中的字母全部转换为小写。
+
+	strings.ToUpper(s string) string：将字符串s中的字母全部转换为大写。
+
+	strings.TrimSpace(s string) string：去除字符串s前后的空格。
+	**/
+
+}
